2257f4e08ff25c1cfca702e980532c86f55bbdcf
Marcel Kraus <mail@marcelkraus.de>
2017-07-21 12:34:01 +0200

    Milestone II: Slider and Variables

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 50
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,19 +23,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+
         let alert = UIAlertController(
             title: "Hello World!",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert
         )
         let action = UIAlertAction (
-            title: "Awesome",
+            title: "OK",
             style: .default,
             handler: nil
         )
         alert.addAction(action)
 
         present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 
 }
