4d77c77f356b6f3fef70f3827e5bae5b18babc41
Marcel Kraus <mail@marcelkraus.de>
2017-07-23 01:02:40 +0200

    Milestone VI: About Screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Marcel Kraus on 23.07.17.
+//  Copyright © 2017 Marcel Kraus. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
